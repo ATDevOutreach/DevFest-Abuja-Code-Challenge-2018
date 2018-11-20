9d5d38c87c68f86faee5b6680dae1c6c6f0f8cf7
Tim <timothyolaleke@gmail.com>
2018-11-20 23:00:33 +0100

    Re-ordered files

--- pusher/settings.py
+++ pusher/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
