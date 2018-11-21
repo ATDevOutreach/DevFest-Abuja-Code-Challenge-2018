58c5ffb8bc443c9c56baac45b13253e563ae7ad3
Tim <timothyolaleke@gmail.com>
2018-11-21 08:00:52 +0100

    Fix hook not fecting & saving

--- messenger/views.py
+++ messenger/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from .models import Hook
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-
+import json
 
 @csrf_exempt
 def process_listen(request):
-    incoming_data = request.POST.get('text')
-    hook = Hook(data=incoming_data)
-    hook.save
-    return HttpResponse(incoming_data)
+    incoming_data = json.loads(request.body)
+    text_data = "{}".format(incoming_data['text'])
+    hook = Hook(data=text_data)
+    hook.save()
+    print(text_data)
+    return HttpResponse(text_data)
