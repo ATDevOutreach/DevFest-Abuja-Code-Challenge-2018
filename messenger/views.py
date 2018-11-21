d04bc043dd30ad045a4d0d32628e8d3677b6e2c6
Tim <timothyolaleke@gmail.com>
2018-11-21 09:16:43 +0100

    Added post

--- messenger/views.py
+++ messenger/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Hook
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 import json
 
 @csrf_exempt
+@require_POST
 def process_listen(request):
     incoming_data = json.loads(request.body)
     text_data = "{}".format(incoming_data['text'])
