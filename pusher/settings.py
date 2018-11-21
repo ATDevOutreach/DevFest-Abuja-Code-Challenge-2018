b6d46bedd1e334b99c6cf60f081262a5ab96ec1d
Tim <timothyolaleke@gmail.com>
2018-11-21 10:37:55 +0100

    Added sentry integration

--- pusher/settings.py
+++ pusher/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://678235b1722d469a93648b22c1727073@sentry.io/1327558",
+    integrations=[DjangoIntegration()]
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
