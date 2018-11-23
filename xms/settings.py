55a32af99265cb9edb7682345f964e5f9a838dbf
Manasseh Mmadu <mmadumanasseh@gmail.com>
2018-11-23 08:44:23 +0100

    adding heroku config

--- xms/settings.py
+++ xms/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 from django.contrib.messages import constants as messages
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
